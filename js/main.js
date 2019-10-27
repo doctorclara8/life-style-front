768921e2090f13c76258fea09237d9d42522659e
doctorclara8 <alionaotp2015@gmail.com>
2019-10-27 19:45:19 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
 $(document).ready(function() {
+    $('.navbar-toggler').on('click', function() {
 
-    $('.first-button').on('click', function() {
-
-        $('.animated-icon1').toggleClass('open');
+        $('.animated-icon').toggleClass('open');
     });
-    // $('.second-button').on('click', function() {
+});
 
-    //     $('.animated-icon2').toggleClass('open');
-    // });
-    // $('.third-button').on('click', function() {
 
-    //     $('.animated-icon3').toggleClass('open');
-    // });
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 600,
+        draggable: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        prevArrow: ".prew",
+        nextArrow: ".next",
+
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 1500,
+                    speed: 600,
+                    touchThreshold: 1,
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    speed: 600,
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
